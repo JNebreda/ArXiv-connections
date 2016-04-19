50b27f842f3d6738501fcedb4fd8f709db4cd0c1
Jenifer Nebreda <jennifer222nm@gmail.com>
2016-04-19 20:20:25 +0200

    Added more normalization of names and authorTree class

--- collaborators.py
+++ collaborators.py
@@ -3,42 +3,77 @@ import urllib
 import feedparser
 from name_normal import normalize_name
 
-def returnCollaborators(author):
+def fetchCollaborators(author):
 
-	# Base api query url
-	base_url = 'http://export.arxiv.org/api/query?';
+  # Base api query url
+  base_url = 'http://export.arxiv.org/api/query?';
 
-	# Search parameters
-	search_query = 'au:%s' % author # searchs for articles authored by nebreda
-	start = 0                   # retreive all results (with a maximum of 1000)
-	max_results = 1000
+  # Search parameters
+  searchTerm = "\""+author+"\"" 
+  search_query = 'au:%s' % searchTerm # search for articles authored by nebreda
+  start = 0                   # retreive all results (with a maximum of 1000)
+  max_results = 300
 
-	query = 'search_query=%s&start=%i&max_results=%i' % (search_query,
+  query = 'search_query=%s&start=%i&max_results=%i' % (search_query,
                                                          start,
                                                          max_results)
 
-	# perform a GET request using the base_url and query
-	response = urllib.urlopen(base_url+query).read()
+  # perform a GET request using the base_url and query
+  response = urllib.urlopen(base_url+query).read()
 
-	# parse the response using feedparser
-	feed = feedparser.parse(response)
+  # parse the response using feedparser
+  feed = feedparser.parse(response)
 
-	# set to store the collaborators
-	collaborators = set()
+  # set to store the collaborators
+  collaborators = set()  
 
-	# Run through each entry, and add collaborator to the set
-	for entry in feed.entries:
+  print(len(feed['entries']))
+  if len(feed['entries']) < max_results:
 
-		for person in entry.authors:
+    # Run through each entry, and add collaborator to the set
+    for entry in feed.entries:
 
-			surname = person.name.split(" ")[-1]
-			collaborators.add(normalize_name(surname))
+      for person in entry.authors:
 
-	collaborators.discard(author)
-	return collaborators
+        # surname = person.name.split(" ")[-1]
+        surname = normalize_name(person.name)
+        if surname:
+          collaborators.add(surname)
+
+    collaborators.discard(author)
+  return collaborators
 
 
+class authorTree():
 
+  def __init__(self, rootObj=None):
+    self.root = rootObj
+    self.children = {}
+
+  def addCollaborator(self, name):
+    self.children[name] = authorTree(name)
+
+  def addAllCollaborators(self):
+    col = fetchCollaborators(self.root)
+    for person in col:
+      self.addCollaborator(person)
+
+  def addNlevels(self,N):
+    self.addAllCollaborators()
+    for child in self.getCollaborators():
+      print('------'+child)
+      childTree = authorTree(child)
+      childTree.addAllCollaborators()
+      self.children[child] = childTree
+      print(childTree.getCollaborators())
+
+
+  def getCollaborators(self):
+    return self.children.keys()
     	
- 
-print(returnCollaborators('Nebreda'))
+  
+myTree = authorTree("J Nebreda")
+myTree.addNlevels(1)
+#myTree.addAllCollaborators()
+#print(myTree.getCollaborators())
+#print(myTree.children["Elvira"].getCollaborators())
--- name_normal.py
+++ name_normal.py
@@ -6,10 +6,19 @@ dictionnary = {
           ord(u'ö'): u'oe',
           ord(u'ü'): u'ue',
           ord(u'ß'): u'ss',
-          #ord(u'.'): None
+          ord(u'.'): None,
+          ord(u':'): None
         }
         
 def normalize_name(name):
-	name = name.translate(dictionnary)
-	name = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
-	return name
+	fullName = name.split()
+	normalizedName = ""
+	if len(fullName) > 1:
+		for i in range(len(fullName)-1):
+			initial = fullName[i][0]
+			normalizedName += initial + " "
+	normalizedName += fullName[-1]
+
+	normalizedName = normalizedName.translate(dictionnary)
+	normalizedName = unicodedata.normalize('NFKD', normalizedName).encode('ascii','ignore')
+	return normalizedName
